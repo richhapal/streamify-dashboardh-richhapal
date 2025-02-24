688ed7c6d013bb30cca9696e44eddc48672721cd
richhapal <richhapal.16jccs098@jietjodhpur.ac.in>
2025-02-24 10:26:49 +0530

    feat: init repo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="navbar p-3 w-full min-h-12 border-b dark:border-neutral-700/70 flex items-center ">
+      <span className="uppercase font-bold text-accent tracking-wide text-xl flex gap-1 items-center">
+        Steamify Dashboard
+      </span>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/index.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default MainLayout;
